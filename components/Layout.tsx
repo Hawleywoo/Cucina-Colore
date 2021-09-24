7ddcfd49167dcd8cb5438c1df01ee4426e4904f5
Hawleywoo <alhawley21@gmail.com>
2021-09-23 23:51:09 -0600

    Styling, image

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,14 +1,21 @@
 import React, { ReactNode } from 'react'
+import Image from 'next/image'
 import Header from './Header'
 import Footer from './Footer'
 
 type Props = {
-    children: ReactNode
+    children: ReactNode,
+    imageAlt: string,
+    imageSrc: string,
+    imageClassName: string,
 }
-export default function Layout({children}: Props) {
+export default function Layout({ children, imageSrc, imageAlt, imageClassName }: Props) {
     return (
         <div>
             <Header />
+            <div style={{width: '100vw'}}>
+                <Image src={imageSrc} alt={imageAlt} className={imageClassName} layout="fill" />
+            </div>
             {children}
             <Footer />
         </div>
