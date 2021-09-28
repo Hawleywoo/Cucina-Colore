5e8e8b2f390ec4ab6554aa19482ad9c8d280eb9d
Hawleywoo <alhawley21@gmail.com>
2021-09-27 23:44:02 -0600

    Icons, font change, fix styling

--- pages/menu.tsx
+++ pages/menu.tsx
@@ -4,7 +4,9 @@ import Layout  from '../components/Layout'
 export default function Menu() {
     return (
         <Layout imageSrc="/img/stock-two-pizzas.jpg" imageAlt="Two pizzas with two glasses of wine" imageClassName="menu__image">
-
+            <div>
+                <h1>Menu</h1>
+            </div>
             
         </Layout>
     )
--- pages/online-ordering.tsx
+++ pages/online-ordering.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Layout from '../components/Layout'
 
 export default function OnlineOrdering() {
+
     return (
         <Layout imageSrc="/img/stock-dark-chef.jpg" imageAlt="Chef with a dark apron cooking" imageClassName="online-ordering__image">
-
-            
+            <div className="online-ordering">
+                <h1>Online Ordering</h1>
+            </div>
         </Layout>
     )
 }
