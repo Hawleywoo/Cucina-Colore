82ebbc2bead0b9e16f975ea25646f9f5ede7d4d2
Hawleywoo <alhawley21@gmail.com>
2021-09-28 23:22:51 -0600

    Styling icons, order onine styling button animations

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,19 +12,43 @@ const Home: NextPage = () => {
             <Image src="/img/stock-chef-topping-pizza.jpg" alt="Chef placing toppings on a pizza fresh out of the oven" layout="fill" objectFit="cover" />
           </div>
           <div className="home__reservations--desc" >
-            <h1>Come Dine</h1> 
-            <h3>  AT ONE OF DENVER&apos;S/CHERRY CREEK&apos;S </h3>
-              <h3>BEST RESTAURANTS</h3>
+            <h1>Come Dine</h1>
+            <h3>AT ONE OF DENVER&apos;S/CHERRY CREEK&apos;S </h3>
+            <h3>BEST RESTAURANTS</h3>
             <p>CUCINA COLORE IS A MODERN TAKE ON TRADITIONAL ITALIAN CUISINE</p>
             <a>MAKE A RESERVATION</a>
           </div>
         </section>
         <section className="home__reviews">
           <div className="home__reviews--icons">
-            <FaGoogle />
-            <FaTripadvisor />
-            <FaFacebookF />
-            <FaYelp />
+            <a
+              href="https://www.google.com/search?q=Cucina+Colore&oq=Cucina+Colore&aqs=chrome..69i57j69i60j69i61l2j69i59l2&sourceid=chrome&ie=UTF-8#lrd=0x876c7e9791d48bcb:0x4c6aa14e166a6675,1,,,"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <FaGoogle />
+            </a>
+            <a
+              href="https://www.tripadvisor.com/Restaurant_Review-g33388-d379503-Reviews-Cucina_Colore-Denver_Colorado.html"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <FaTripadvisor />
+            </a>
+            <a
+              href="https://www.facebook.com/pg/cucinacolorerestaurant/reviews/?ref=page_internal"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <FaFacebookF />
+            </a>
+            <a
+              href="https://www.yelp.com/biz/cucina-colore-denver"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <FaYelp />
+            </a>
           </div>
           <h2>CHECK OUT OUR REVIEWS</h2>
         </section>
--- pages/online-ordering.tsx
+++ pages/online-ordering.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 import Layout from '../components/Layout'
 
 export default function OnlineOrdering() {
@@ -6,9 +7,28 @@ export default function OnlineOrdering() {
     return (
         <Layout imageSrc="/img/stock-dark-chef.jpg" imageAlt="Chef with a dark apron cooking" imageClassName="online-ordering__image">
             <div className="online-ordering">
-                <h1>Online Ordering</h1>
-                <a href='https://app.upserve.com/s/cucina-colore-denver'>ORDER NOW</a>
+                <div className="online-ordering__desc">
+                    <h1><span>Online</span></h1>
+                    <h1>ORDERING</h1>
+                    <p>Order some of the finest food in Cherry Creek and pick up in person.</p>
+                    <div className="btn-container">
+                        <div className="btn">
+                            <a
+                                href='https://app.upserve.com/s/cucina-colore-denver'
+                                target="_blank"
+                                rel="noreferrer noopener"
+                            >
+                                ORDER NOW
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className="online-ordering__second-image">
+                </div>
             </div>
+            <section className="online-ordering__images-contianer">
+                <p> images of food</p>
+            </section>
         </Layout>
     )
 }
