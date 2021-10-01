0bb70b7baa4181d19b3b6030fdfa1d7d265255f2
Hawleywoo <alhawley21@gmail.com>
2021-09-30 23:11:54 -0600

    Parse csv wine list

--- pages/menu.tsx
+++ pages/menu.tsx
@@ -1,13 +1,33 @@
 import React from 'react'
 import Layout  from '../components/Layout'
+import Script from 'next/script'
+import Papa from 'papaparse';
+
 
 export default function Menu() {
+
+    const getData = async () => {
+        const data = Papa.parse(await fetchCsv())
+        console.log(data)
+        return data
+    }
+
+    const fetchCsv = async () => {
+        const response: any = await fetch('/menus/WINE BTB LIST APRIL 2021 first page.csv')
+        const reader: any = response.body.getReader();
+        const result = await reader.read();
+        const decoder: any = new TextDecoder('utf-8')
+        const csv = await decoder.decode(result.value);
+        console.log(csv)
+        return csv
+    }
+    getData()
+
     return (
         <Layout imageSrc="/img/stock-two-pizzas.jpg" imageAlt="Two pizzas with two glasses of wine" imageClassName="menu__image">
             <div>
                 <h1>Menu</h1>
             </div>
-            
         </Layout>
     )
 }
