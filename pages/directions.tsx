57faeef7bdbbde276f934ad1d4018eb942ea9f21
Hawleywoo <alhawley21@gmail.com>
2021-10-20 23:12:18 -0600

    Styling and layout directions

--- pages/directions.tsx
+++ pages/directions.tsx
@@ -1,18 +1,33 @@
 import React from 'react'
-import Layout  from '../components/Layout'
+import Layout from '../components/Layout'
 import Map from '../components/Map'
 
 export default function Directions() {
     return (
         <Layout imageSrc="/img/stock-wine-fruit.jpg" imageAlt="Wine and glasses with grapes" imageClassName="directions__image">
             <div className="directions">
-                <h1>Directions</h1>
-                <div>
-                    ADDRESS AND CONTACT INFO
+                <div className="directions__header-container">
+                    <h1>Directions</h1>
+                    <div className="directions__header-container__address">
+                        <div>
+                            <h3><b>ADDRESS:</b></h3>
+                            <p>3041 East Third Ave.
+                                Cherry Creek North
+                                Denver, CO 80206</p>
+                        </div>
+                        <div>
+                            <h3><b>CONTACT:</b></h3>
+                            <p>303-393-6917</p>
+                        </div>
+                        <div>
+                            <h3><b>HOURS:</b></h3>
+                            <p>MON-SAT: 11am - 10pm
+                                SUN: 11am-9pm</p>
+                        </div>
+                    </div>
                 </div>
-                <Map/>
+                <Map />
             </div>
         </Layout>
     )
 }
-// AIzaSyCCZJhgNybS2UZE3y_O4f0pTewuJ20joCw
